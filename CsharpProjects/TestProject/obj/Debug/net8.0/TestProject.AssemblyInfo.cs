122de8885d51c344a8857376b367be22a964a07b
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-06-13 11:01:00 -0700

    Challenge Project setup.

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343c3b1a35b5be3745f5ba760ec233b9ef8da780")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c62a505963ef757dbc4733f56aad534612cf229")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
