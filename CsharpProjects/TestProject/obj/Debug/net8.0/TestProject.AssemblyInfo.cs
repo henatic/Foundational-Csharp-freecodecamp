418d86e36c13ed5357a03829417904e91e32de96
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-06-17 10:07:49 -0700

    Assembly Info.

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee4c4e672f63a68e5bdaa8bb4687eae5a0af149b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7f0a1f1d40614d97602fcbef8e0597d43ccad5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
