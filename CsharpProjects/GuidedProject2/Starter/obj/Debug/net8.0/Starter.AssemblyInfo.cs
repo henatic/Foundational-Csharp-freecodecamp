d92d364b17294aa59884a41dc7540400dfb327fc
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-06-19 16:30:07 -0700

    Animal exercises complete.

--- CsharpProjects/GuidedProject2/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ CsharpProjects/GuidedProject2/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2fe57b776879a1f2bc0fc13f70dc2bfe392aaeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57940c74b04e6e22f585c376bd1893f5aa73975e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
