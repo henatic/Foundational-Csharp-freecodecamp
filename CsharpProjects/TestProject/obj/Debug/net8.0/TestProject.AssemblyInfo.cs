b516d2ac6ca7f811698b63a4b08754eb1462a556
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-06-27 14:19:17 -0700

    Method module complete.

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8cafe62de7583409494cfa24172e6ca38de84d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558f1840ea75a5ef543593d65ed43e3406895b16")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
